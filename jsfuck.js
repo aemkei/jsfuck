31ad19dc10ada67c52123251339bb8eb97793ebd
Alexander I. Zaytsev <hazzik@gmail.com>
2014-01-15 11:10:23 +1300

    Optimize `C`

--- jsfuck.js
+++ jsfuck.js
@@ -53,7 +53,7 @@
 
     'A':   '(+false+Array)[10]',
     'B':   '(+false+Boolean)[10]',
-    'C':   'Function("return escape")()("<")[2]',
+    'C':   'Function("return escape")()(("")["italics"]())[2]',
     'D':   'Function("return escape")()("=")[2]',
     'E':   '(RegExp+"")[12]',
     'F':   '(+false+Function)[10]',
