09b431905724482f2a9f990d4596fa4f36bfbaec
Martin Kleppe <kleppe@ubilabs.net>
2016-06-10 17:38:47 +0200

    Add some detailed output for exploring new methods.

--- test/jsfuck_test.js
+++ test/jsfuck_test.js
@@ -8,13 +8,18 @@ var JSFuck = require('../jsfuck.js').JSFuck,
 
 		test.strictEqual(value, unencoded, 'encoding "' + value + '" failed');
 	};
+
+
 var MIN = 32, MAX = 127;
 var fs = require('fs');
 
 exports['integration'] = {
 	'test': function(test) {
 		var file = fs.openSync('output.txt', 'w+'),
-			simple = "";
+			simple = "`[]=+";
+
+		simple += "j<>/\"\n";
+
 
 		for (var i = MIN; i < MAX; i++) {
 			var c = String.fromCharCode(i),
@@ -28,6 +33,80 @@ exports['integration'] = {
 		}
 
 		fs.closeSync(file);
+
+		function getProps(o){
+			return Object.getOwnPropertyNames(o) || [];
+		}
+
+		function logValue(property, value) {
+
+			if (typeof value === "undefined"){
+				value = "undefined".red;
+			}
+
+			value = value + "";
+			value = value.replace(/\s?\n\s?/g, "");
+
+			var isSpecial = value.split("").find(function(char) {
+				return !simple.includes(char);
+			});
+
+			if (isSpecial){
+				value = value.magenta + isSpecial;
+			} else {
+				value = value.grey;
+			}
+
+			console.log("  ", property.green, ":", value);
+		}
+
+		var types = [simple, 12, [23], false, [].slice];
+
+		types.forEach(function(instance){
+			var constructor = instance.constructor,
+				proto = Object.getPrototypeOf(instance),
+
+				properties = []
+					.concat(getProps(instance))
+					.concat(getProps(proto))
+					.filter(function(name) {
+						return name.split('').every(function(char){
+							return simple.includes(char);
+						});
+					});
+
+			console.log("\n" + (constructor.name + "").yellow);
+
+			properties.forEach(function(property) {
+				var value = "XXX".red;
+				value = instance[property];
+				logValue(property, value);
+
+				if (typeof instance[property] === "function"){
+					try {
+						value = instance[property]();
+						logValue(property + "()", value);
+					} catch(e) { }
+				}
+
+
+				try {
+					value = proto[property];
+					if (value){
+						logValue(property + "*", value);
+					}
+				} catch(e) {}
+
+				try {
+					if (typeof proto[property] === "function"){
+						value = instance[property]();
+						if (value) {
+							logValue(property + "*()", value);
+						}
+					}
+				} catch(e) { }
+			});
+		});
 
 		console.log("\nSIMPLE CHARS:" + simple);
 
